4b03a90c18b59ea6b2f8c75714c8cc4e85b82d53
hardeep85singh <hardeep85singh@gmail.com>
2021-04-05 20:24:16 +0530

    commit on 05.04.21

---
+++ src/main/java/trees/TreeProblems.java
@@ -0,0 +1,124 @@
+package trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreeProblems {
+
+    public static class Node{
+        Node left;
+        Node right;
+        int data;
+
+        public Node(int data){
+            left = null;
+            right = null;
+            this.data = data;
+        }
+    }
+
+    public static int findMaximumRecursive(Node root){
+        int maxValue = Integer.MIN_VALUE;
+        if(root != null){
+            int leftMax = findMaximumRecursive(root.left);
+            int rightMax = findMaximumRecursive(root.right);
+            if(leftMax > rightMax){
+                maxValue = leftMax;
+            } else {
+                maxValue = rightMax;
+            }
+
+            if(root.data > maxValue){
+                maxValue = root.data;
+            }
+        }
+        return maxValue;
+    }
+
+    public static int findMaximumLevelOrderTraversal(Node root){
+        if(root == null){
+            throw new IllegalArgumentException("No element to find");
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            Node temp = queue.remove();
+            if(temp.data > max){
+                max = temp.data;
+            }
+            if(temp != null){
+                if(temp.left != null){
+                    queue.offer(temp.left);
+                }
+                if(temp.right != null){
+                    queue.offer(temp.right);
+                }
+            }
+        }
+        return max;
+    }
+
+    public static boolean findElementRecursive(Node root, int data){
+        if(root == null){
+            return false;
+        }
+        if(root.data == data){
+            return true;
+        }
+        return findElementRecursive(root.left, data) || findElementRecursive(root.right, data);
+    }
+
+    public static boolean findElementLevelOrderTraversal(Node root, int data){
+        if(root == null){
+            return false;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            Node temp = queue.remove();
+            if(temp.data == data){
+                return true;
+            }
+            if(temp != null){
+                if(temp.left != null){
+                    queue.offer(temp.left);
+                }
+                if(temp.right != null){
+                    queue.offer(temp.right);
+                }
+            }
+        }
+        return false;
+    }
+
+    public Node insertLeverOrder(Node root, int data){
+        if(root == null){
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            Node temp = queue.poll();
+            if(temp != null){
+                if(temp.left != null){
+                    queue.offer(temp.left);
+                } else{
+                    temp.left = new Node(data);
+                    return root;
+                }
+
+                if(temp.right != null){
+                    queue.offer(temp.right);
+                } else {
+                    temp.right = new Node(data);
+                    return root;
+                }
+            }
+
+        }
+        return root;
+    }
+}
