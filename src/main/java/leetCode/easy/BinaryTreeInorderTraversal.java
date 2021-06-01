1a0e596039005f79ec3a9e083f1a69dabad80aff
hardeep85singh <hardeep85singh@gmail.com>
2021-06-01 21:48:38 +0530

    Commit on 01.05.2021

---
+++ src/main/java/leetCode/easy/BinaryTreeInorderTraversal.java
@@ -0,0 +1,55 @@
+package leetCode.easy;
+
+import trees.InOrderTraversalTree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+
+        if(root != null){
+            inorderTraversal(root.left);
+            result.add(root.val);
+            inorderTraversal(root.right);
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[]args){
+        TreeNode node = new TreeNode(10);
+        node.left = new TreeNode(20);
+        node.right = new TreeNode(30);
+        node.left.left = new TreeNode(40);
+        node.left.right = new TreeNode(50);
+        node.right.left = new TreeNode(60);
+        node.right.right = new TreeNode(70);
+
+     System.out.println(inorderTraversal(node));
+
+    }
+
+}
